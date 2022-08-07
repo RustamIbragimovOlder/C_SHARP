34637091baa1e55052c142d449fc66bd4418e164
Rustam Ibragimov <rustamibragimov67@yandex.ru>
2022-08-07 10:09:38 +0500

    Внесены правки в решение задач ПЗ №1

--- C_Sharp_PracticalTasks/C_Sharp_PracticalTask1_6авг22/Task2/Program.cs
+++ C_Sharp_PracticalTasks/C_Sharp_PracticalTask1_6авг22/Task2/Program.cs
@@ -10,19 +10,19 @@ int Prompt(string message)
     return result;                          // Возвращает результат
 }
 
-int value1;                                  // Объявление переменной
-int value2;                                  // Объявление переменной
-int value3;                                  // Объявление переменной
-int max;
+int value1;                                  // Объявление переменной 1
+int value2;                                  // Объявление переменной 2
+int value3;                                  // Объявление переменной 3
+int max;                                     // Объявление переменной max
 
 value1 = Prompt("Введите число 1 -> ");       // Вводим значение 1-го числа
 value2 = Prompt("Введите число 2 -> ");       // Вводим значение 2-го числа
 value3 = Prompt("Введите число 3 -> ");       // Вводим значение 3-го числа
 
-max = value1;                                 // назначение max
+max = value1;                                 // Стартовое назначение max
 
 if (value2 > value1) max = value2;            // Сравнение данных
 if (value3 > value2) max = value3;            // Сравнение данных
 {
-    System.Console.WriteLine($"max = {max}"); // Вывод результата
+    System.Console.WriteLine($"{max}"); // Вывод результата
 }
