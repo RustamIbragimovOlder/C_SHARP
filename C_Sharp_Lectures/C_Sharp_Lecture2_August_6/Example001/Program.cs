5fdee5e740e0df848b41af63a7207e5259619278
Rustam Ibragimov <rustamibragimov67@yandex.ru>
2022-08-08 12:37:19 +0500

    Добавлены задачи лекции 2

---
+++ C_Sharp_Lectures/C_Sharp_Lecture2_August_6/Example001/Program.cs
@@ -0,0 +1,26 @@
+﻿int Max(int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+    if(arg2 > result) result = arg2;
+    if(arg3 > result) result = arg3;
+    return result;
+}
+
+
+int a1 = 26; // Исходные данные
+int b1 = 87;
+int c1 = 34;
+int a2 = 44888;
+int b2 = 12555;
+int c2 = 98;
+int a3 = 253;
+int b3 = 73;
+int c3 = 21;
+
+// int max1 = Max(a1, b1, c1);
+// int max2 = Max(a2, b2, c2);
+// int max3 = Max(a3, b3, c3);
+
+int max = Max(Max(a1, b1, c1), Max(a2, b2, c2), Max(a3, b3, c3));
+
+System.Console.WriteLine(max);
---
+++ C_Sharp_Lectures/C_Sharp_Lecture2_August_6/Example002Array/Program.cs
@@ -0,0 +1,17 @@
+﻿int Max(int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+    if (arg2 > result) result = arg2;
+    if (arg3 > result) result = arg3;
+    return result;
+}
+
+int[] array = { 2656, 87, 34, 44, 12556, 98, 253, 73, 21 }; // Исходные данные
+
+int max = Max(
+    Max(array[0], array[1], array[2]),
+    Max(array[3], array[4], array[5]),
+    Max(array[6], array[7], array[8])
+);
+
+System.Console.WriteLine(max);
