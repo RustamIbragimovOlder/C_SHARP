f9c1799e3038a136e201b57b353b3c57d35693e4
Rustam Ibragimov <rustamibragimov67@yandex.ru>
2022-11-03 18:11:53 +0500

    Добавлены сортировки

---
+++ BootCamp/Cорт Выбором/Program.cs
@@ -0,0 +1,93 @@
+﻿/* Смысл:
+считаем первое число минимальным и сравниваем его с другими элементами
+если находим меньшее чем минимальное - записываем его на позицию 0
+[6, 15, 2, 9, -3]
+
+1 этап
+MIN = 6
+6 < 15
+6 > 2
+MIN = 2
+2 < 9
+2 > -3
+MIN = -3
+[-3, 6, 15, 2, 9]
+
+2 этап
+MIN = 6
+6 < 15
+6 > 2
+MIN = 2
+2 < 9
+[-3, 2, 6, 15, 9]
+
+3 этап
+MIN = 6
+6 < 15
+6 < 9
+[-3, 2, 6, 15, 9]
+
+4 этап
+MIN = 15
+15 > 9
+[-3, 2, 6, 9, 15]
+*/
+
+
+// Пример кода с числами
+// Console.WriteLine("Введите кол-во элементов массива:");
+// int n = Convert.ToInt32(Console.ReadLine());
+// // Заполнение массива
+// int[] array = new int[n];
+// for (int i = 0; i < n; i++)
+// {
+//     Console.Write("Введите число: ");
+//     array[i] = Convert.ToInt32(Console.ReadLine());
+// }
+// Console.WriteLine();
+// Console.WriteLine("Начальный массив: [" + string.Join(", ", array) + "]");
+// // Cортировка
+// for (int i = 0; i < n - 1; i++)
+// {
+//     int MinIndex = i;
+//     for (int j = i + 1; j < n; j++)
+//     {
+//         if (array[j] < array[MinIndex])
+//             MinIndex = j; 
+//     }
+//     int temp;
+//     temp = array[MinIndex];
+//     array[MinIndex] = array[i];
+//     array[i] = temp;
+// }
+// Console.WriteLine("Конечный массив: [" + string.Join(", ", array) + "]");
+
+
+
+// то же самое со строками (сортирует по длине строки)
+string[] array = new string[5];
+for (int i = 0; i < 5; i++)
+{
+    array[i] = Console.ReadLine();
+}
+
+Console.Write("[" + string.Join(", ", array) + "]");
+for (int i = 0; i < 4; i++)
+{
+    int MinIndex = i;
+    for (int j = i + 1; j < 5; j++)
+    {
+        if (array[j].Length < array[MinIndex].Length)
+            MinIndex = j; 
+    }
+    string temp;
+    temp = array[MinIndex];
+    array[MinIndex] = array[i];
+    array[i] = temp;
+}
+Console.WriteLine("Конечный массив: [" + string.Join(", ", array) + "]");
+
+
+
+
+
---
+++ BootCamp/Быстрая сорт/Program.cs
@@ -0,0 +1,64 @@
+﻿/* Квик-сорт
+y = n * log(n)
+arr = [1, 0, -6, -1, 5, 3, 2]
+1 шаг - инициализация массива
+2 шаг - выбор опорного элемента (пивот)
+как правило - первый или последний, но можно другой
+сейчас - последний
+pivot = arr[6]
+- все элементы меньше pivot должны стоять левее pivot
+- все элементы больше pivot должны стоять правее pivot
+arr = [1, 0, -6, -1, 2, 5, 3]    
+3 шаг - повторить шаги 1 и 2 для левого и правого подмассивов
+
+левый подмассив [1, 0, -6, -1]
+pivot = -1 -> [-6, -1, 1, 0]
+левый подмассив [-6]
+правый подмассив [1, 0]
+pivot = 0 -> [0, 1]
+правый подмассив [1]
+на выходе -> [-6, -1, 0, 1]
+
+правый подмассив [5, 3]
+pivot = 3 -> [3, 5]
+
+итог -> [-6, -1, 0, 1, 2, 3, 5]
+
+*/
+
+int[] arr = { 0, -5, 2, 3, 5, 9, -1, 7, -10, 15, 4, -4 };
+QuickSort(arr, 0, arr.Length - 1);
+Console.Write($"Отсортированный массив [{string.Join(", ", arr)}]");
+Console.WriteLine();
+
+void QuickSort(int[] inputArray, int minIndex, int maxIndex)
+{
+    if (minIndex >= maxIndex) return;
+    int pivot = GetPivotIndex(inputArray, minIndex, maxIndex);
+    QuickSort(inputArray, minIndex, pivot - 1);
+    QuickSort(inputArray, pivot + 1, maxIndex);
+    return;
+}
+
+int GetPivotIndex(int[] inputArray, int minIndex, int maxIndex)
+{
+    int pivotIndex = minIndex - 1;
+    for (int i = minIndex; i <= maxIndex; i++)
+    {
+        if (inputArray[i] < inputArray[maxIndex])
+        {
+            pivotIndex++;
+            Swap(inputArray, i, pivotIndex);
+        }
+    }
+    pivotIndex++;
+    Swap(inputArray, pivotIndex, maxIndex);
+    return pivotIndex;
+}
+
+void Swap(int[] inputArray, int leftValue, int rightValue)
+{
+    int temp = inputArray[leftValue];
+    inputArray[leftValue] = inputArray[rightValue];
+    inputArray[rightValue] = temp;
+}
